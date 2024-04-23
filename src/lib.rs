5c6658c11718fa42abef02425cd67e9504cec84d
Howard <139238193+howard-oc@users.noreply.github.com>
2024-04-23 19:06:42 +0100

    fix:bug: linter (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod pipeline;
 pub mod promise;
-use log;
 use crate::pipeline::PipelineConfig;
-use std::{env, process};
 use dotenv::dotenv;
+use log;
+use std::{env, process};
 
 // Structure to hold potential errors
 #[derive(Debug)]
@@ -11,11 +11,7 @@ struct EnvVarError {
     var_name: String,
 }
 
-
-
-pub fn run_pipeline(args:Vec<String>) {
-
-   
+pub fn run_pipeline(args: Vec<String>) {
     dotenv().ok();
 
     // Validate environment variables up front
@@ -30,12 +26,11 @@ pub fn run_pipeline(args:Vec<String>) {
         }
     }
 
-    
     if args.len() < 2 {
         log::warn!("Usage: <command> [build, pipeline, load, push, rmi, pull]");
         process::exit(1);
     }
-    
+
     // Extract validated environment variables
     let workflow_type = env::var("KRATIX_WORKFLOW_TYPE").unwrap();
     let base_instance = env::var("BASE_INSTANCE").unwrap();
@@ -44,49 +39,51 @@ pub fn run_pipeline(args:Vec<String>) {
     let kratix_input_dir = env::var("KRATIX_INPUT").unwrap();
     let kratix_output_dir = env::var("KRATIX_OUTPUT").unwrap();
 
-
     let config = PipelineConfig::new(
-            &base_instance,
-            &res_dir,
-            &dep_dir,
-            &kratix_output_dir,
-            &kratix_input_dir,
-            &workflow_type);
+        &base_instance,
+        &res_dir,
+        &dep_dir,
+        &kratix_output_dir,
+        &kratix_input_dir,
+        &workflow_type,
+    );
 
     log::debug!("<- Start Pipeline ({}) ->", config.workflow_type());
-
-
 
     match config.workflow_type() {
         "promise" => {
             // Fullful promise.yaml
             if let Err(err) =
                 // tmp/transfer/dependecies -> /kratix/output
-                pipeline::copy_files(config.dep_dir(), config.kratix_output_dir()) {
+                pipeline::copy_files(config.dep_dir(), config.kratix_output_dir())
+            {
                 log::warn!("Error during file copy: {}", err);
             }
-        },
+        }
         "resource" => {
             log::debug!("  1. transform resource");
             // Fullfil resource_request.yaml
-            promise::transform(config.res_dir(),
-                               config.base_instance(),
-                               config.kratix_output_dir(),
-                               config.kratix_input_dir());
-        },
+            promise::transform(
+                config.res_dir(),
+                config.base_instance(),
+                config.kratix_output_dir(),
+                config.kratix_input_dir(),
+            );
+        }
         "request" => {
             log::debug!("  1. transform request");
             // Fullfil resource_request.yaml
-            promise::transform(config.res_dir(),
-                               config.base_instance(),
-                               config.kratix_output_dir(),
-                               config.kratix_input_dir());
+            promise::transform(
+                config.res_dir(),
+                config.base_instance(),
+                config.kratix_output_dir(),
+                config.kratix_input_dir(),
+            );
         }
         _ => {
             log::error!("No workflow_type");
         }
     }
-
 
     //pipeline::status();
 
@@ -94,7 +91,6 @@ pub fn run_pipeline(args:Vec<String>) {
 
     log::debug!("<- End Pipeline ->");
 }
-
 
 // validation function
 fn validate_env_vars() -> Result<(), Vec<EnvVarError>> {
